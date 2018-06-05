f8d565cc5b64870ca7f8308befbe600245ba4429
Oleksandr Antoniuk <antonukom@gmail.com>
2018-06-05 18:32:16 +0300

    async load from api

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -2,27 +2,11 @@ import React, {PropTypes} from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as courseActions from '../../actions/courseActions';
+import CourseList from './CourseList';
 
 class CoursesPage extends React.Component {
     constructor(props, context) {
       super(props, context);
-
-      this.state = {
-        course: { title: "" }
-      };
-
-      this.onTitleChange = this.onTitleChange.bind(this);
-      this.onSave = this.onSave.bind(this);
-    }
-
-    onTitleChange(event) {
-      const course = this.state.course;
-      course.title = event.target.value;
-      this.setState({course: course});
-    }
-
-    onSave(event) {
-      this.props.actions.createCourse(this.state.course);
     }
 
     courseRow(course, index) {
@@ -30,22 +14,15 @@ class CoursesPage extends React.Component {
     }
 
     render() {
-        return (
-            <div>
-                <h1>Courses</h1>
-                {this.props.courses.map(this.courseRow)}
-                <h3>Add course</h3>
-                <input
-                  type="text"
-                  onChange={this.onTitleChange}
-                  value={this.state.course.title} />
-
-                <input
-                  type="submit"
-                  value="Save"
-                  onClick={this.onSave} />
-            </div >
-        );
+      debugger;
+      const {courses} = this.props;
+      
+      return (
+          <div>
+              <h1>Courses</h1>
+              <CourseList courses={courses}/>
+          </div >
+      );
     }
 }
 
